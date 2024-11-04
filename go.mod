e80fd98bbfdebba65a64cd1247e9112fcba8d95a
Literally Kirby <56140289+LiterallyKirby@users.noreply.github.com>
2024-11-04 10:12:19 -0600

    Add files via upload

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.23.2
 require golang.org/x/crypto v0.28.0
 
 require (
-	github.com/inancgumus/screen v0.0.0-20190314163918-06e984b86ed3 // indirect
+	github.com/inancgumus/screen v0.0.0-20190314163918-06e984b86ed3
 	golang.org/x/sys v0.26.0 // indirect
 	golang.org/x/term v0.25.0 // indirect
 )
--- main.go
+++ main.go
@@ -7,18 +7,24 @@ import (
 	"os"
 	"os/exec"
 	"shell/cc"
+	"strconv"
 	"strings"
 
 	"github.com/inancgumus/screen"
 )
 
-var cc_list = []string{"encrypt", "decrypt"}
+var cc_list = []string{"encrypt", "decrypt", "cd", "find", "history"}
+var c_history = []string{}
 
 func main() {
 	screen.Clear()
-	for {
 
-		fmt.Print("$ ")
+	for {
+		wd, err := os.Getwd()
+		if err != nil {
+			fmt.Printf("failed to find wd, while the program will continue I would check that out %v\n", err)
+		}
+		fmt.Print(wd, " $ ")
 		reader := bufio.NewReader(os.Stdin)
 
 		pre_command, err := reader.ReadString('\n')
@@ -29,6 +35,45 @@ func main() {
 
 		// Trim newline characters
 		pre_command = strings.TrimSpace(pre_command)
+
+		// Detect special cases for history navigation
+		if pre_command == "!!" && len(c_history) > 0 {
+			// Repeat the last command
+			pre_command = c_history[len(c_history)-1]
+			fmt.Println(pre_command) // Show repeated command for user visibility
+		} else if strings.HasPrefix(pre_command, "!") {
+			// Run a specific history command by index
+			numStr := pre_command[1:]
+			num, err := strconv.Atoi(numStr)
+			if err == nil && num > 0 && num <= len(c_history) {
+
+				fmt.Printf("We are about to run the command: %s\n", c_history[num-1])
+				fmt.Println("Are you SURE you want to do this? Y/N: ")
+				reader := bufio.NewReader(os.Stdin)
+				for {
+					sure, err := reader.ReadString('\n')
+					if err != nil {
+						fmt.Println("Choice must be a string")
+						continue
+					}
+					sure = strings.TrimSpace(sure)
+					if sure == "y" || sure == "Y" {
+						pre_command = c_history[num-1]
+						break
+					} else if sure == "n" || sure == "N" {
+						fmt.Println("We won't run the command then")
+						break
+					} else {
+						fmt.Println("That was not an option :/ try that again")
+						continue
+					}
+				}
+
+			} else {
+				fmt.Println("No command found for that history index")
+				continue
+			}
+		}
 
 		if pre_command == "" {
 			fmt.Println("Command can't be empty")
@@ -43,6 +88,9 @@ func main() {
 			run_cc(command)
 		} else {
 			run_command(command)
+		}
+		if pre_command != "history" {
+			c_history = append(c_history, pre_command)
 		}
 	}
 }
@@ -64,7 +112,14 @@ func run_cc(command []string) {
 		cc.Encrypt(command)
 	case "decrypt":
 		cc.Decrypt(command)
+	case "cd":
+		cc.ChangeDir(command)
+	case "find":
+		cc.Find(command)
+	case "history":
+		cc.History(c_history)
 	}
+
 }
 
 func run_command(command []string) {
